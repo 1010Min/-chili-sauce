47a707e7712cdd528ce8638ca3b872e8b35517f7
ssook1222 <chlthr1222@naver.com>
2021-11-07 03:04:37 +0900

    UPDATE : 리팩터링

--- static/checkbox.js
+++ static/checkbox.js
@@ -14,3 +14,28 @@ function getCheckboxValue()  {
     console.log(result)
     document.getElementById('result').innerText = result;
 }
+
+$(document).ready(function () {
+    showReview();
+});
+function showReview() {
+    $.ajax({
+        type: "GET",
+        url: "/review",
+        data: {},
+        success: function (response) {
+            let reviews=response['all_reviews']
+            for(let i=0;i<reviews.length;i++){
+                let keyword_raw = reviews[i]['keyword']
+                for(let j=0;j<keyword_raw.length;j++){
+                    let keyword=keyword_raw[j]
+                    let temp_html= `<input type='checkbox'
+                        name='animal'
+                        value=${keyword}
+                        onclick='getCheckboxValue()'/> ${keyword}`
+                    $('#reviews-box').append(temp_html)
+                }
+            }
+        }
+    })
+}
