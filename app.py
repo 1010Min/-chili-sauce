4d75ee477b74915b826fc642546a71f1acd55f4d
ssook1222 <chlthr1222@naver.com>
2021-11-07 02:57:34 +0900

    UPDATE : DB 데이터 get으로 가져옴

--- app.py
+++ app.py
@@ -37,6 +37,11 @@ def trackinfo3():
 def trackinfo4():
    return render_template('4.html')
 
+@app.route('/review', methods=['GET'])
+def read_reviews():
+    reviews=list(db.track.find({},{'_id':False}))
+    return jsonify({'all_reviews': reviews})
+
 if __name__ == '__main__':
    app.run('0.0.0.0',port=5000,debug=True)
    
